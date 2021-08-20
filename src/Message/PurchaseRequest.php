7a258ed160ec8bd33d29ab8bdbe640f467bf918d
Chris Wright <chriswright@live.com>
2021-08-20 14:03:04 -0400

    feat(charge): adds billing details

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -22,6 +22,8 @@ class PurchaseRequest extends AbstractRequest
             'currency' => $this->getCurrency(),
         ];
 
+        $this->addBillingData($data);
+
         return $data;
     }
 
@@ -48,5 +50,65 @@ class PurchaseRequest extends AbstractRequest
             'cvc' => $card->getCvv(),
             'number' => $card->getNumber(),
         ];
+    }
+
+    /**
+     * Adds the billing data.
+     *
+     * @param array $data
+     */
+    protected function addBillingData(array &$data): void
+    {
+        /** @var CreditCard $creditCard */
+        if ($creditCard = $this->getCard()) {
+            // A card is present, so include billing details
+            if ($creditCard->getEmail()) {
+                $data['email'] = $creditCard->getEmail();
+            }
+
+            if ($creditCard->getBillingFirstName()) {
+                $data['first'] = $creditCard->getBillingFirstName();
+            }
+
+            if ($creditCard->getBillingLastName()) {
+                $data['last'] = $creditCard->getBillingLastName();
+            }
+
+            if ($creditCard->getBillingCompany()) {
+                $data['company'] = $creditCard->getBillingCompany();
+            }
+
+            if ($creditCard->getBillingAddress1()) {
+                $data['address']['line1'] = $creditCard->getBillingAddress1();
+            }
+
+            if ($creditCard->getBillingAddress2()) {
+                $data['address']['line1'] = $creditCard->getBillingAddress2();
+            }
+
+            if ($creditCard->getBillingCity()) {
+                $data['address']['city'] = $creditCard->getBillingCity();
+            }
+
+            if ($creditCard->getBillingPostcode()) {
+                $data['address']['postal_code'] = $creditCard->getBillingPostcode();
+            }
+
+            if ($creditCard->getBillingState()) {
+                $data['address']['state'] = $creditCard->getBillingState();
+            }
+
+            if ($creditCard->getBillingCountry()) {
+                $data['address']['country'] = $creditCard->getBillingCountry();
+            }
+
+            if ($creditCard->getBillingPhone()) {
+                $data['phone'] = $creditCard->getBillingPhone();
+            }
+
+            if ($creditCard->getBillingCountry()) {
+                $data['address']['country'] = $creditCard->getBillingCountry();
+            }
+        }
     }
 }
