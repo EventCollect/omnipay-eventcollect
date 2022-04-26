9a2e495eb0b9105cdd2cdc009728a7569788eabc
Chris Wright <chriswright@live.com>
2022-04-26 13:44:54 -0400

    fix(customer): store address on create (#7)
    * Fixed address is not stored when creating a customer
    
    * Removed first and name last params in favour of card name

--- src/Message/CustomerCreateRequest.php
+++ src/Message/CustomerCreateRequest.php
@@ -21,38 +21,45 @@ class CustomerCreateRequest extends AbstractRequest
         return $this->setParameter('email', $value);
     }
 
-    public function getFirstName(): string
-    {
-        return $this->getParameter('first_name');
-    }
-
-    public function setFirstName(string $value): self
-    {
-        return $this->setParameter('first_name', $value);
-    }
-
-    public function getLastName(): string
-    {
-        return $this->getParameter('last_name');
-    }
-
-    public function setLastName(string $value): self
-    {
-        return $this->setParameter('last_name', $value);
-    }
-
     /**
      * @return string[]
      * @throws InvalidRequestException
      */
     public function getData(): array
     {
-        $this->validate('email', 'first_name', 'last_name');
+        $this->validate('email');
+
+        $creditCard = $this->getCard();
 
         return [
+            'billing' => $this->addBillingData(),
             'email' => $this->getEmail(),
-            'first' => $this->getFirstName(),
-            'last' => $this->getLastName(),
+            'first' => $creditCard->getBillingFirstName(),
+            'last' => $creditCard->getBillingLastName(),
         ];
+    }
+
+    /**
+     * Adds the billing data.
+     *
+     * @return array
+     */
+    protected function addBillingData(): array
+    {
+        $creditCard = $this->getCard();
+        if (!$creditCard) {
+            return [];
+        }
+
+        return array_filter([
+            'address' => array_filter([
+                'line1' => $creditCard->getBillingAddress1(),
+                'line2' => $creditCard->getBillingAddress2(),
+                'city' => $creditCard->getBillingCity(),
+                'postal_code' => $creditCard->getBillingPostcode(),
+                'state' => $creditCard->getBillingState(),
+                'country' => $creditCard->getBillingCountry(),
+            ]),
+        ]);
     }
 }
