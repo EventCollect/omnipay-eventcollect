7f73feffedbc0eaed23a2a1dae0ae85642d03238
Chris Wright <chriswright@live.com>
2022-04-26 11:20:22 -0400

    Fixed address is not stored when creating a customer

--- src/Message/CustomerCreateRequest.php
+++ src/Message/CustomerCreateRequest.php
@@ -50,9 +50,34 @@ class CustomerCreateRequest extends AbstractRequest
         $this->validate('email', 'first_name', 'last_name');
 
         return [
+            'billing' => $this->addBillingData(),
             'email' => $this->getEmail(),
             'first' => $this->getFirstName(),
             'last' => $this->getLastName(),
         ];
+    }
+
+    /**
+     * Adds the billing data.
+     *
+     * @return array
+     */
+    protected function addBillingData(): array
+    {
+        $creditCard = $this->getCard();
+        if (!$creditCard) {
+            return [];
+        }
+
+        return array_filter([
+            'address' => array_filter([
+                'line1' => $creditCard->getBillingAddress1(),
+                'line2' => $creditCard->getBillingAddress2(),
+                'city' => $creditCard->getBillingCity(),
+                'postal_code' => $creditCard->getBillingPostcode(),
+                'state' => $creditCard->getBillingState(),
+                'country' => $creditCard->getBillingCountry(),
+            ]),
+        ]);
     }
 }
