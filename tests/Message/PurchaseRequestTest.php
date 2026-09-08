9552fca470b67ccc9ee2f3047de68f515b23cd7e
Chris Wright <chriswright@live.com>
2026-09-08 11:23:35 -0400

    Updated tests

--- tests/Message/PurchaseRequestTest.php
+++ tests/Message/PurchaseRequestTest.php
@@ -2,6 +2,8 @@
 
 namespace League\EventCollect\Test\Message;
 
+use Omnipay\EventCollect\BankAccount;
+use Omnipay\EventCollect\Exception\InvalidBankAccountException;
 use Omnipay\EventCollect\Message\PurchaseRequest;
 use Omnipay\Tests\TestCase;
 
@@ -112,5 +114,88 @@ class PurchaseRequestTest extends TestCase
 
         $this->assertSame('First', $data['billing']['first']);
         $this->assertSame('Address 1', $data['billing']['address']['line1']);
+    }
+
+    public function testDataWithBankAccount(): void
+    {
+        $this->request->setCurrency('USD');
+        $this->request->setBankAccount($this->getValidBankAccount());
+
+        $data = $this->request->getData();
+
+        $this->assertSame([
+            'account_number' => '123456789',
+            'routing_number' => '021000021',
+            'account_type' => 'checking',
+            'account_holder_type' => 'individual',
+        ], $data['bank_account']);
+        $this->assertArrayNotHasKey('card', $data);
+    }
+
+    public function testDataWithBankAccountObject(): void
+    {
+        $this->request->setBankAccount(new BankAccount($this->getValidBankAccount()));
+
+        $data = $this->request->getData();
+
+        $this->assertSame('021000021', $data['bank_account']['routing_number']);
+    }
+
+    public function testDataWithBankAccountBilling(): void
+    {
+        $this->request->setBankAccount($this->getValidBankAccount());
+        $this->request->setBillingFirstName('First');
+        $this->request->setBillingCompany('Acme Inc');
+
+        $data = $this->request->getData();
+
+        $this->assertSame('First', $data['billing']['first']);
+        $this->assertSame('Acme Inc', $data['billing']['company']);
+    }
+
+    public function testDataWithSourceIgnoresBankAccount(): void
+    {
+        $this->request->setSource('source');
+        $this->request->setBankAccount($this->getValidBankAccount());
+
+        $data = $this->request->getData();
+
+        $this->assertSame('source', $data['source']);
+        $this->assertArrayNotHasKey('bank_account', $data);
+    }
+
+    public function testDataWithInvalidBankAccount(): void
+    {
+        $this->request->setCurrency('USD');
+        $this->request->setBankAccount(array_merge($this->getValidBankAccount(), [
+            'routingNumber' => '021000022',
+        ]));
+
+        $this->expectException(InvalidBankAccountException::class);
+
+        $this->request->getData();
+    }
+
+    public function testSendBankAccountSuccess(): void
+    {
+        $this->request->setAmount('50.00');
+        $this->request->setCurrency('USD');
+        $this->request->setBankAccount($this->getValidBankAccount());
+
+        $this->setMockHttpResponse('PurchaseBankAccountSuccess.txt');
+        $response = $this->request->send();
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertSame('8f4d1e02-5b7c-4a19-9e63-2c8f0a5d7b31', $response->getTransactionReference());
+    }
+
+    private function getValidBankAccount(): array
+    {
+        return [
+            'accountNumber' => '123456789',
+            'routingNumber' => '021000021',
+            'accountType' => BankAccount::ACCOUNT_TYPE_CHECKING,
+            'accountHolderType' => BankAccount::HOLDER_TYPE_INDIVIDUAL,
+        ];
     }
 }
